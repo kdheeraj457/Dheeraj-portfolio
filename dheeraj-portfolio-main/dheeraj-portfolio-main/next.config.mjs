afa8abae5998f28dfabd6457b7edbc185395d88d
kdheeraj457 <167636643+kdheeraj457@users.noreply.github.com>
2025-08-07 15:26:42 +0530

    Add files via upload

---
+++ dheeraj-portfolio-main/dheeraj-portfolio-main/next.config.mjs
@@ -0,0 +1,17 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+};
+
+// module.exports = nextConfig;
+export default nextConfig;
+
